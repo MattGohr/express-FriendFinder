82a4f85e0deb78b84593780cbadd96c7da6152ac
Matt Gohr <gohr89@gmail.com>
2018-03-05 18:45:45 -0500

    working basic app

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,8 +4,8 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+var tableData = require("../data/friends");
+// var waitListData = require("../data/waitinglistData");
 
 
 // ===============================================================================
@@ -19,12 +19,8 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function(req, res) {
+  app.get("/api/friends", function(req, res) {
     res.json(tableData);
-  });
-
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
   });
 
   // API POST Requests
@@ -35,7 +31,7 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
+  app.post("/api/friends", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
@@ -44,8 +40,8 @@ module.exports = function(app) {
       res.json(true);
     }
     else {
-      waitListData.push(req.body);
-      res.json(false);
+      // waitListData.push(req.body);
+      // res.json(false);
     }
   });
 
