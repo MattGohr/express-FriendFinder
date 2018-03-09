42740b00deace050a6d47eae494af57998e2d5f9
Matt Gohr <gohr89@gmail.com>
2018-03-08 20:38:23 -0500

    added form fill validation

--- app/public/scripts.js
+++ app/public/scripts.js
@@ -1,43 +1,68 @@
 console.log('script loded');
 
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
 
-$(".submit").on("click", function(event) {
-  event.preventDefault();
 
+$("#submit").submit(function( event ) {
+  // event.preventDefault();
   // Here we grab the form elements
-  var newReservation = {
-    customerName: $("#reserve-name").val().trim(),
-    phoneNumber: $("#reserve-phone").val().trim(),
-    customerEmail: $("#reserve-email").val().trim(),
-    customerID: $("#reserve-unique-id").val().trim()
-  };
+  var newFried = {
+    name: $("#name").val(),
+    photo: $("#pic").val(),
+    score: [
+      $("#question1").val(),
+      $("#question2").val(),
+      $("#question3").val(),
+      $("#question4").val(),
+      $("#question5").val(),
+      $("#question6").val(),
+      $("#question7").val(),
+      $("#question8").val(),
+      $("#question9").val(),
+      $("#question10").val(),
+    ]
+  }
 
-  console.log(newReservation);
+  console.log(newFried);
 
   // This line is the magic. It"s very similar to the standard ajax function we used.
   // Essentially we give it a URL, we give it the object we want to send, then we have a "callback".
   // The callback is the response of the server. In our case, we set up code in api-routes that "returns" true or false
   // depending on if a tables is available or not.
 
-  $.post("/api/tables", newReservation,
+  $.post("/api/friends", newFried,
     function(data) {
 
       // If a table is available... tell user they are booked.
       if (data) {
-        alert("Yay! You are officially booked!");
+        console.log("Yay! You are officially booked!");
       }
 
       // If a table is available... tell user they on the waiting list.
       else {
-        alert("Sorry you are on the wait list");
+        console.log("Sorry you are on the wait list");
       }
 
-      // Clear the form when submitting
-      $("#reserve-name").val("");
-      $("#reserve-phone").val("");
-      $("#reserve-email").val("");
-      $("#reserve-unique-id").val("");
 
     });
+  $.get("/api/friends", function(data) {
+    console.log(data);
+  });
 
 });
